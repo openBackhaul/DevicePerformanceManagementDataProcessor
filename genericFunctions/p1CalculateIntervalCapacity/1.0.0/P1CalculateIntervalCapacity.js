d5551ea6c83e6764b4d42d8ad71b7979661e9d99
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-03-23 15:40:34 +0100

    Adding genericFunctions: p1CalculateIntervalCapacity Version 1.0.0

---
+++ genericFunctions/p1CalculateIntervalCapacity/1.0.0/P1CalculateIntervalCapacity.js
@@ -0,0 +1,66 @@
+
+const p1CalculateIntervalCapacity = (input) => {
+  try {
+
+    const timeXstatesList = input["time-xstates-list"];
+    const transmissionModeList = input["transmission-mode-list"];
+
+    if (timeXstatesList === undefined) {
+      throw "timeXstatesList not provided";
+    }
+
+    if (!Array.isArray(timeXstatesList)) {
+      throw "timeXstatesList invalid";
+    }
+
+    if (transmissionModeList === undefined) {
+      throw "transmissionModeList not provided";
+    }
+
+    if (!Array.isArray(transmissionModeList)) {
+      throw "transmissionModeList invalid";
+    }
+
+    const capacityMap = new Map();
+
+    for (const mode of transmissionModeList) {
+      capacityMap.set(mode["transmission-mode"], mode.capacity);
+    }
+
+    let totalOfferedVolume = 0;
+    let totalTime = 0;
+
+    for (const state of timeXstatesList) {
+
+      const capacity = capacityMap.get(state["transmission-mode"]);
+
+      if (capacity === undefined) {
+        continue;
+      }
+
+      const time = state.time;
+
+      const offeredVolume = capacity * time;
+
+      totalOfferedVolume += offeredVolume;
+      totalTime += time;
+    }
+
+    const intervalCapacity =
+      totalTime === 0 ? 0 : Math.round(totalOfferedVolume / totalTime);
+
+    return {
+      "interval-capacity": intervalCapacity
+    };
+
+  } catch (err) {
+
+    if (typeof err === "string") {
+      throw err;
+    }
+
+    throw "General processing error";
+  }
+}
+
+module.exports = p1CalculateIntervalCapacity;
