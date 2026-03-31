5c4dbb1674d3cb9015b354642180bc02c2150ef3
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-03-31 17:48:32 +0200

    To be implement function p1RemoveDefaultValues Fixes #142

--- genericFunctions/p1RemoveDefaultValues/1.0.0/P1RemoveDefaultValues.js
+++ genericFunctions/p1RemoveDefaultValues/1.0.0/P1RemoveDefaultValues.js
@@ -1,10 +1,61 @@
 
 const ERRORS = require ('./ErrorsEnum');
 
+let defaultValuesList =[];
+
 const p1RemoveDefaultValues = (input) => {
   try {
     const parameters = input["parameters"];
     const inputObj = input["input-object"];
+
+    // Starting parameter validation
+    if (parameters === undefined && inputObj == undefined) {
+      return ERRORS.GENERAL_ERROR
+    }
+
+    if (parameters === undefined) {
+      return ERRORS.PARAMS_NOT_PROVIDED;
+    }
+
+    if (inputObj === undefined) {
+      return ERRORS.INPUTOBJ_NOT_PROVIDED;
+    }
+
+    if (Object.keys(inputObj).length === 0 && Object.keys(parameters).length === 0) {
+      return ERRORS.GENERAL_ERROR;
+    } else if (Object.keys(inputObj).length === 0) {
+      return ERRORS.INPUTOBJ_INVALID;
+    } else if (Object.keys(parameters).length === 0) {
+      return ERRORS.PARAMS_INVALID;
+    }
+
+    // Fill default key list
+    for (const [key, value] of Object.entries(parameters)) {
+      if (defaultValuesList.includes({ 'attribute-name': key, 'attribute-value': value })) {
+        console.log("Parameter already present: " + key);
+      } else {
+        console.log("Add the parameter: " + key);
+      }
+    }
+
+    let cleanedObject = JSON.parse(JSON.stringify(inputObj)); // Initializate return value
+
+    for (const [key, value] of Object.entries(parameters)) {
+      console.log(`${key}: ${value}`);
+      if (cleanedObject[key] != undefined) {
+        if (cleanedObject[key] == value) {
+          console.log("Remove parameter: " + key);
+          delete cleanedObject[key];
+        } else {
+          console.log("Keep the parameter: " + key);
+        }
+      }
+      
+    }
+
+    return {
+      "cleaned-object": cleanedObject
+    };
   } catch (e) {
     return ERRORS.GENERAL_ERROR;
   }
--- genericFunctions/p1RemoveDefaultValues/1.0.0/P1RemoveDefaultValues.test.js
+++ genericFunctions/p1RemoveDefaultValues/1.0.0/P1RemoveDefaultValues.test.js
@@ -8,4 +8,99 @@ describe('p1RemoveDefaultValues', () => {
     .toBe(ERRORS.GENERAL_ERROR);
   });
 
+  test('Passing undefined, return general error', () => {
+    expect(p1RemoveDefaultValues(undefined))
+    .toBe(ERRORS.GENERAL_ERROR);
+  });
+
+  test('Passing empty object, return general error', () => {
+    expect(p1RemoveDefaultValues({}))
+    .toBe(ERRORS.GENERAL_ERROR);
+  });
+
+  test('Passing both undefined, return general error', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': undefined,
+      'input-object': undefined,
+    }))
+    .toBe(ERRORS.GENERAL_ERROR);
+  });
+
+  test('Passing only parameters property, return input object not provided', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': {},
+      // 'input-object': // something,
+    }))
+    .toBe(ERRORS.INPUTOBJ_NOT_PROVIDED);
+  });
+
+  test('Passing only input-object property, return parameter not provided', () => {
+    expect(p1RemoveDefaultValues({
+      // 'parameters': // something,
+      'input-object': {},
+    }))
+    .toBe(ERRORS.PARAMS_NOT_PROVIDED);
+  });
+
+  test('Passing properties but both empty objects, return general error', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': {},
+      'input-object': {},
+    }))
+    .toBe(ERRORS.GENERAL_ERROR);
+  });
+
+  test('Passing only input-object property, return input object not provided', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': { 'abc-parameter': 0,},
+      'input-object': {},
+    }))
+    .toBe(ERRORS.INPUTOBJ_INVALID);
+  });
+
+  test('Passing only parameters property, return input object not provided', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': {},
+      'input-object': { 'abc-parameter': 0,},
+    }))
+    .toBe(ERRORS.PARAMS_INVALID);
+  });
+
+  test('Passing parameters and input-object property, return input object not provided', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': { 'abc-parameter': 0 },
+      'input-object': { 'abc-parameter': 0}
+    }))
+    .toStrictEqual({
+      "cleaned-object": {}
+    });
+  });
+
+  test('Passing parameters and input-object property, return input object not provided', () => {
+    expect(p1RemoveDefaultValues({
+      'parameters': {
+        'abc-parameter': 10,
+        '123-parameter': 20,
+        'xyz-parameter': 30,
+        'other-parameter': "SIAE",
+        'default-parameter': "Milano"
+      },
+      'input-object': {
+        'abc-parameter': 1,
+        '123-parameter': 2,
+        'xyz-parameter': 3,
+        'other-parameter': "Lorenzo",
+        'default-parameter': "Milano"
+      }
+    }))
+    .toStrictEqual({
+      "cleaned-object": {
+        'abc-parameter': 1,
+        '123-parameter': 2,
+        'xyz-parameter': 3,
+        'other-parameter': "Lorenzo"
+        // 'default-parameter': "Milano"
+      }
+    });
+  });
 });
