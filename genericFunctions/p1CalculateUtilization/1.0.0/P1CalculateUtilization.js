9797b275ff5e793b10d637cebdcaf681c7bf1014
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-04-20 14:58:34 +0200

    Add comment and remove variables add try catch clause

--- genericFunctions/p1CalculateUtilization/1.0.0/P1CalculateUtilization.js
+++ genericFunctions/p1CalculateUtilization/1.0.0/P1CalculateUtilization.js
@@ -17,90 +17,141 @@ const GRAN_UNKN = 'GRANULARITY_PERIOD_TYPE_PERIOD-UNKNOWN';
 const GRAN_NOTDEF = 'GRANULARITY_PERIOD_TYPE_PERIOD-NOT_YET_DEFINED';
 
 // Aggregates the interval capacity of all transporting AirInterfaces
+// input
+// - logical-termination-point
+// - physical-server-ltp-list
+// - period-end-time
+// 
+// Errors:
+// - 'logicalTerminationPoint list not provided'
+// - 'logicalTerminationPoint list invalid'
+// - 'physicalServerLtpList not provided'
+// - 'physicalServerLtpList invalid'
+// - 'periodEndTime not provided'
+// - 'periodEndTime invalid'
+// - 'totalAirInterfaceIntervalCapacity could not be provided'
+// - 'General processing error'
 function calculateTotalAirInterfaceIntervalCapacity(input) {
-  const ltpList = input[LTP]; // array
-  const psyServerLTP = input[PSYSERVERLTP]; // array
-  const periodEndTime = input[ENDTIME]; // string
+  try {
+    const ltpList = input[LTP];               // array
+    const psyServerLTP = input[PSYSERVERLTP]; // array
+    const periodEndTime = input[ENDTIME];     // string
 
-  if (ltpList == null) {
-    return ERRORS.LTP_LIST_NOT_PROVIDED;
+    if (ltpList == null) {
+      return ERRORS.LTP_LIST_NOT_PROVIDED;
+    }
+    // TODO: manage 'logicalTerminationPoint list invalid'
+
+    if (psyServerLTP == null) {
+      return ERRORS.PSY_SERVER_LTP_LIST_NOT_PROVIDED;
+    }
+    // TODO: manage 'physicalServerLtpList invalid'
+
+    if (periodEndTime == null) {
+      return ERRORS.PERIOD_ENDTIME_NOT_PROVIDED;
+    }
+    // TODO: manage 'periodEndTime invalid'
+
+    // Filter out LTP no in ServerList
+    const cleanLTPlist = ltpList.filter((ltp) => psyServerLTP.includes(ltp['uuid']));
+
+    // Reduce function to calculate all AirInterfaceCapacity for the aggration
+    const totalAirIfCap = cleanLTPlist.reduce((accLTP, currentLTP) => {
+      const lp = currentLTP['layer-protocol'];
+      const resLP = lp.reduce((accLP, currentLP) => {
+        // Pick-up historical performance data array
+        const histPerfList = currentLP['air-interface-2-0:air-interface-pac']['air-interface-historical-performances']['historical-performance-data-list'];
+        // Filter out data that is not related to air-interface 15 minutes and should match period end time
+        const histDataClean = histPerfList.filter((perfData) => perfData[ENDTIME] == periodEndTime && perfData['granularity-period'] == `air-interface-2-0:${GRAN_15MIN}`);
+        // Sum all interval-capacity
+        const resHistory = histDataClean.reduce((accHis, currHistory) => accHis += currHistory[PERFDATA]['interval-capacity'], 0);
+
+        return accLP + resHistory;
+      }, 0); // 0 is initial accumulator
+      return accLTP + resLP;
+    }, 0); // 0 is initial accumulator
+
+    // 'Sum of the intervalCapacity of all AirInterfaces in the aggregation group that is transporting this EthernetContainer in kbps
+    // from [sum of all {[/logical-termination-point={physical-server-ltp-list[*]}/layer-protocol=*/air-interface-2-0:air-interface-pac/air-interface-historical-performances/historical-performance-data-list={$input.period-end-time}/performance-data/interval-capacity}]
+    //             with {[/logical-termination-point={physical-server-ltp-list[*]}/layer-protocol=*/air-interface-2-0:air-interface-pac/air-interface-historical-performances/historical-performance-data-list={$input.period-end-time}/granularity-period]}==air-interface-2-0:GRANULARITY_PERIOD_TYPE_PERIOD-15-MIN'
+
+    // TODO: manage totalAirInterfaceIntervalCapacity could not be provided'
+    return {
+      'total-air-interface-interval-capacity': totalAirIfCap
+    };
+  } catch (err) {
+    return ERRORS.GENERAL_ERROR;
   }
-  // TODO: manage 'logicalTerminationPoint list invalid'
 
-  if (psyServerLTP == null) {
-    return ERRORS.PSY_SERVER_LTP_LIST_NOT_PROVIDED;
-  }
-  // TODO: manage 'physicalServerLtpList invalid'
-
-  if (periodEndTime == null) {
-    return ERRORS.PERIOD_ENDTIME_NOT_PROVIDED;
-  }
-  // TODO: manage 'periodEndTime invalid'
-
-  const timeStamp = new Date(periodEndTime);
-
-  let sumTot = 0;
-
-  let cleanLTPlist = ltpList.filter((ltp) => psyServerLTP.includes(ltp['uuid']));
-
-
-  let totalAirIfCap = cleanLTPlist.reduce((accLTP, currentLTP) => {
-    let lp = currentLTP['layer-protocol'];
-    let resLP = lp.reduce((accLP, currentLP) => {
-      let histPerfList = currentLP['air-interface-2-0:air-interface-pac']['air-interface-historical-performances']['historical-performance-data-list'];
-      let histDataClean = histPerfList.filter((perfData) => perfData[ENDTIME] == periodEndTime && perfData['granularity-period'] == `air-interface-2-0:${GRAN_15MIN}`);
-
-      let resHistory = histDataClean.reduce((accHis, currHistory) => accHis += currHistory[PERFDATA]['interval-capacity'], 0);
-      // acc+= sum;
-      return accLP + resHistory;
-    }, 0);
-    return accLTP + resLP;
-  }, 0);
-
-  // 'Sum of the intervalCapacity of all AirInterfaces in the aggregation group that is transporting this EthernetContainer in kbps
-  // from [sum of all {[/logical-termination-point={physical-server-ltp-list[*]}/layer-protocol=*/air-interface-2-0:air-interface-pac/air-interface-historical-performances/historical-performance-data-list={$input.period-end-time}/performance-data/interval-capacity}]
-  //             with {[/logical-termination-point={physical-server-ltp-list[*]}/layer-protocol=*/air-interface-2-0:air-interface-pac/air-interface-historical-performances/historical-performance-data-list={$input.period-end-time}/granularity-period]}==air-interface-2-0:GRANULARITY_PERIOD_TYPE_PERIOD-15-MIN'
-
-  // TODO: manage totalAirInterfaceIntervalCapacity could not be provided'
-  return {
-    'total-air-interface-interval-capacity': totalAirIfCap
-  };
 }
 
 // Calculates the utilization
+// input
+// - total-bytes-output
+// - total-air-interface-interval-capacity
+// - time-period
+// 
+// Errors:
+// - 'totalBytesOutput not provided'
+// - 'totalBytesOutput invalid'
+// - 'totalAirInterfaceIntervalCapacity not provided'
+// - 'totalAirInterfaceIntervalCapacity invalid'
+// - 'timePeriod not provided'
+// - 'timePeriod invalid'
+// - 'utilization could not be provided'
+// - 'General processing error'
 function calculateUtilization(input) {
-  const totalByteOutput = input[TOTBYTEOUT]; //string
-  const totalAirIfIntCap = input[TOTAIRIFCAP]; // integer
-  const timePeriod = input[TIMEPERIOD]; // integer
+  try {
+    const totalByteOutput = input[TOTBYTEOUT];   //string
+    const totalAirIfIntCap = input[TOTAIRIFCAP]; // integer
+    const timePeriod = input[TIMEPERIOD];        // integer
 
-  if (totalByteOutput == null) {
-    return ERRORS.TOTAL_BYTE_OUTPUT_NOT_PROVIDED;
+    if (totalByteOutput == null) {
+      return ERRORS.TOTAL_BYTE_OUTPUT_NOT_PROVIDED;
+    }
+    // TODO: manage 'totalBytesOutput invalid'
+
+    if (totalAirIfIntCap == null) {
+      return ERRORS.TOTAL_AIR_IF_INT_CAP_NOT_PROVIDED;
+    }
+    // TODO: manage totalAirInterfaceIntervalCapacity invalid
+
+    if (timePeriod == null) {
+      return ERRORS.TIME_PERIOD_NOT_PROVIDED;
+    }
+    // TODO: manage 'timePeriod invalid'
+
+    // From Spec file: interface.yaml
+    //    'Interval utilization in %
+    //    from [ {total-bytes-output}*8 / ( {total-air-interface-interval-capacity}*1000 * {time-period} ) ]'
+    const calcNum = Number(totalByteOutput) * 8;
+    const calcDen = totalAirIfIntCap * 1000 * timePeriod;
+    const result = (calcNum / calcDen) * 100;
+
+    // Return result
+    return {
+      'utilization': result
+    };
+  } catch (err) {
+    return ERRORS.GENERAL_ERROR;
   }
-  // TODO: manage 'totalBytesOutput invalid'
-
-  if (totalAirIfIntCap == null) {
-    return ERRORS.TOTAL_AIR_IF_INT_CAP_NOT_PROVIDED;
-  }
-  // TODO: manage totalAirInterfaceIntervalCapacity invalid
-
-  if (timePeriod == null) {
-    return ERRORS.TIME_PERIOD_NOT_PROVIDED;
-  }
-  // TODO: manage 'timePeriod invalid'
-
-  // From Spec file: interface.yaml
-  //    'Interval utilization in %
-  //    from [ {total-bytes-output}*8 / ( {total-air-interface-interval-capacity}*1000 * {time-period} ) ]'
-  const calcNum = Number(totalByteOutput) * 8;
-  const calcDen = totalAirIfIntCap * 1000 * timePeriod;
-  const result = (calcNum / calcDen) * 100;
-
-  // Return result
-  return {
-    'utilization': result
-  };
 }
 
+// Calculates utilization of the aggregated physical resources in a performance data slice
+// input:
+// - historical-performance-data
+// - aggregation-group
+// - result-cc
+// 
+// Errors - Utilization added:
+// - 'historicalPerformanceData not provided'
+// - 'historicalPerformanceData invalid'
+// - 'aggregationGroup not provided'
+// - 'aggregationGroup invalid'
+// - 'result-cc not provided'
+// - 'result-cc invalid'
+// - 'Utilization could not be added'
+// - 'General processing error'
 const p1CalculateUtilization = (input) => {
   try {
     const historicalPerfData = input[HISTPERFDATA]; // Object
