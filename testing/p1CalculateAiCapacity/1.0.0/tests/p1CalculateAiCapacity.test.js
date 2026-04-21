58372dde51d94ec1661b6c72fdf7ca76a2dbb75d
redchy <redouane.chyiy.ext@alten.com>
2026-04-21 11:48:18 +0100

    add testcases for p1CalculateAiCapacity

---
+++ testing/p1CalculateAiCapacity/1.0.0/tests/p1CalculateAiCapacity.test.js
@@ -0,0 +1,17 @@
+/**
+ * AUTO-GENERATED FILE. DO NOT EDIT.
+ * Source: testing\p1CalculateAiCapacity\1.0.0\scenarios.yaml
+ */
+
+const path = require("path");
+const { runFunctionVersionFromScenarios } = require("../../../tools/functionTestRunner");
+
+const repoRoot = path.resolve(__dirname, "../../../..");
+
+describe("p1CalculateAiCapacity 1.0.0 (generated)", () => {
+  runFunctionVersionFromScenarios({
+    repoRoot,
+    functionName: "p1CalculateAiCapacity",
+    version: "1.0.0",
+  });
+});
