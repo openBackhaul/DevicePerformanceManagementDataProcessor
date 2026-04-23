6f3fb064837740a672822a328ee78b3a82a859bf
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-04-23 16:42:20 +0200

    fix out of range temperature fields checks

--- genericFunctions/p1RemoveOutOfRangeTemperature/1.0.0/P1RemoveOutOfRangeTemperature.js
+++ genericFunctions/p1RemoveOutOfRangeTemperature/1.0.0/P1RemoveOutOfRangeTemperature.js
@@ -36,6 +36,12 @@ function validateParameters(paramArray) {
     parameterStruct[paramElem['parameter-name']] = paramElem['value'];
   });
 
+  paramAllowed.forEach(paramElem => {
+    if (!parameterStruct.hasOwnProperty(paramElem)) {
+      res = false;
+    }
+  });
+
   return res;
 }
 
