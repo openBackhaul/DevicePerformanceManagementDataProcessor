00c22ec908cfffbebc837774244a212455b1f8be
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-04-30 19:27:09 +0200

    update jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   projects: [
-    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt/1.0.0'
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt'
   ],
 };
