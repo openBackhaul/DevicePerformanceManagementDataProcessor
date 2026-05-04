6805aa1e7886f1f0376fae9ae229c0b36b418955
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-05-04 12:35:06 +0200

    Update Jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -7,8 +7,8 @@ module.exports = {
     '<rootDir>/genericFunctions/p1CalculateIntervalCapacity',
     '<rootDir>/genericFunctions/p1CalculateAiCapacity',
     '<rootDir>/genericFunctions/p1CalculateUtilization',
-    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1CalculateEthernetKpis/1.0.0',
-    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt/1.0.0',
-    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputOnf/1.0.0'
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1CalculateEthernetKpis',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputOnf'
   ],
 };
