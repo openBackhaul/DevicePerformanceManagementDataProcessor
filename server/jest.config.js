cbae996ee2862f603f1664ce04495cecf46bf649
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-05-04 11:23:26 +0200

    Move package.json and jest config inside server folder

---
+++ server/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projects: [
+    '<rootDir>/genericFunctions/p1RemoveOutOfRangeTemperature',
+    '<rootDir>/genericFunctions/p1RemoveOutOfRangeLevels',
+    '<rootDir>/genericFunctions/p1RemoveDefaultValues',
+    '<rootDir>/genericFunctions/p1DiscardIrrelevantPmRecords',
+    '<rootDir>/genericFunctions/p1CalculateIntervalCapacity',
+    '<rootDir>/genericFunctions/p1CalculateAiCapacity',
+    '<rootDir>/genericFunctions/p1CalculateUtilization',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1CalculateEthernetKpis/1.0.0',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt/1.0.0',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputOnf/1.0.0'
+  ],
+};
