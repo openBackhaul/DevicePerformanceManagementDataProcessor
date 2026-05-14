102064ee595df7649d360ea1f0fd5be931c8b766
mishra-siae <prakhar.b.mishra@capgemini.com>
2026-05-14 17:32:04 +0530

    Implement Function p1IterateEcPmSlices and Unit Tests

---
+++ server/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1IterateEcPmSlices/1.0.0/ErrorsEnum.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // General
+  GENERAL_ERROR: 'General processing error',
+
+  // Parameters
+  PARAMETERS_NOT_PROVIDED: 'parameters not provided',
+  PARAMETERS_INVALID: 'parameters invalid',
+
+  // Historical data list
+  HISTORICAL_DATA_LIST_NOT_PROVIDED: 'historicalPerformanceDataList not provided',
+  HISTORICAL_DATA_LIST_INVALID: 'historicalPerformanceDataList invalid',
+
+  // Processing errors (as per YAML contract)
+  KPI_CALCULATION_FAILED: 'Ethernet KPIs could not be calculated',
+  DEFAULT_VALUES_REMOVAL_FAILED: 'Default values could not be removed',
+  UTILIZATION_CALCULATION_FAILED: 'Utilization could not be calculated',
+
+  // Output errors
+  HISTORICAL_DATA_LIST_OUTPUT_FAILED: 'historicalPerformanceDataList could not be provided',
+
+  // Most recent timestamps
+  MOST_RECENT_PERIOD_END_TIME_NOT_PROVIDED: 'mostRecentPeriodEndTime not provided',
+  MOST_RECENT_PERIOD_END_TIME_INVALID: 'mostRecentPeriodEndTime invalid',
+
+  MOST_RECENT_PERIOD_END_TIME_24_NOT_PROVIDED: 'mostRecentPeriodEndTime24 not provided',
+  MOST_RECENT_PERIOD_END_TIME_24_INVALID: 'mostRecentPeriodEndTime24 invalid',
+
+  MOST_RECENT_PERIOD_END_TIME_FAILED: 'mostRecentPeriodEndTime could not be provided',
+  MOST_RECENT_PERIOD_END_TIME_24_FAILED: 'mostRecentPeriodEndTime24 could not be provided',
+};
