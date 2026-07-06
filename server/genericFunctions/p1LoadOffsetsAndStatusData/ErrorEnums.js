ca0649f270e52f23df7332e69c0bc765e396b969
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-06 11:43:03 +0200

    Adding scafolding for function p1LoadOffsetsAndStatusData

---
+++ server/genericFunctions/p1LoadOffsetsAndStatusData/ErrorEnums.js
@@ -0,0 +1,13 @@
+const ERRORS = {
+  DATA_STORE_URL_NOT_PROV: "dataStoreUrl not provided",
+  DATA_STORE_URL_INVALID: "dataStoreUrl invalid",
+  
+  MOUNT_NAME_NOT_PROVIDED: "mountName not provided",
+  MOUNT_NAME_INVALID: "mountName invalid",
+
+  ELK_READ_ERROR: "ElasticSearch read error",
+
+  GENERAL_ERROR: "General processing error"
+}
+
+module.exports = ERRORS;
---
+++ server/genericFunctions/p1LoadOffsetsAndStatusData/P1LoadOffsetsAndStatusData.js
@@ -0,0 +1,11 @@
+const ERRORS = require('./ErrorsEnum');
+
+const p1LoadOffsetsAndStatusData = (input) => {
+  try {
+
+  } catch (error) {
+    return ERRORS.GENERAL_ERROR;
+  }
+}
+
+module.exports = p1LoadOffsetsAndStatusData;
