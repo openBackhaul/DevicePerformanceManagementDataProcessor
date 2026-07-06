5ee30b1a813562246ce687a95cba1955ca952f70
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-06 11:37:47 +0200

    Adding scafolding for function p1CalculateBusyHourPerfomanceIndicators

---
+++ server/genericFunctions/p1CalculateBusyHourPerformanceIndicators/ErrorEnums.js
@@ -0,0 +1,14 @@
+const ERRORS = {
+  HISTORICAL_PERF_NOT_PROVIDED = "historicalPerformanceData not provided",
+  HISTORICAL_PERF_INVALID = "historicalPerformanceData invalid",
+  HISTORICAL_PERF_WRONG_GRAN_PROV = "historicalPerformanceData of wrong granularity provided",
+
+  INT_STATUS_NOT_PROVIDED = "interfaceStatus not provided",
+  INT_STATUS_INVALID = "interfaceStatus invalid",
+
+  BUSY_HOUR_KPIS_COULDNT_ADDED = "busy hour KPIs could not be added",
+
+  GENERAL_ERROR: "General processing error"
+}
+
+module.exports = ERRORS;
---
+++ server/genericFunctions/p1CalculateBusyHourPerformanceIndicators/P1CalculateBusyHourPerformanceIndicators.js
@@ -0,0 +1,11 @@
+const ERRORS = require('./ErrorsEnum');
+
+const p1CalculateBusyHourPerformanceIndicators = (input) => {
+  try {
+
+  } catch (error) {
+    return ERRORS.GENERAL_ERROR;
+  }
+}
+
+module.exports = p1CalculateBusyHourPerformanceIndicators;
