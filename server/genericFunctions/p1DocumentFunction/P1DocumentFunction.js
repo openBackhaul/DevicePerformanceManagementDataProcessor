b6ba7cf2c59fcdfc13f6e09fb0b2480bc628a4c6
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-06 23:46:36 +0200

    Adding p1Document function

---
+++ server/genericFunctions/p1DocumentFunction/P1DocumentFunction.js
@@ -0,0 +1,16 @@
+const ERRORS = require('./ErrorsEnum');
+
+/**
+ * p1DocumentFunction
+ *
+ * Creates plain text documentation for active functions only.
+ */
+function p1DocumentFunction(input) {
+  try {
+
+  } catch (error) {
+    return ERRORS.GENERAL_ERROR;
+  }
+}
+
+module.exports = p1DocumentFunction;
