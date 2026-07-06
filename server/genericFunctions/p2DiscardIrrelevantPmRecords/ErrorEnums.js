250b39809e9493f08b042a0bfc402767e1cff8c4
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-06 15:49:43 +0200

    Update Errors enum of p2DiscardIrrelevantPmRecords

--- server/genericFunctions/p2DiscardIrrelevantPmRecords/ErrorEnums.js
+++ server/genericFunctions/p2DiscardIrrelevantPmRecords/ErrorEnums.js
@@ -1,21 +1,33 @@
 const ERRORS = {
-  UUID_NOT_PROVIDED = "uuid not provided",
-  UUID_INVALID = "uuid invalid",
+  HISTORICAL_PERF_NOT_PROVIDED: "historicalPerformanceDataList not provided",
+  HISTORICAL_PERF_INVALID: "historicalPerformanceDataList invalid",
 
-  FORMER_MRPET_NOT_PROVIDED = "formerMostRecentPeriodEndTime not provided",
-  FORMER_MRPET_INVALID = "formerMostRecentPeriodEndTime invalid",
+  FORMER_MRPET_NOT_PROVIDED: "formerMostRecentPeriodEndTime not provided",
+  FORMER_MRPET_INVALID: "formerMostRecentPeriodEndTime invalid",
 
-  NEW_MRPET_NOT_PROVIDED = "newMostRecentPeriodEndTime not provided",
-  NEW_MRPET_NOT_INVALID = "newMostRecentPeriodEndTime invalid",
+  FORMER_MRPET24_NOT_PROVIDED: "formerMostRecentPeriodEndTime24 not provided",
+  FORMER_MRPET24_INVALID: "formerMostRecentPeriodEndTime24 invalid",
 
-  AMOUNTREC_NOT_PROVIDED = "amountReceived not provided",
-  AMOUNTREC_INVALID = "amountReceived invalid",
+  HISTORICAL_PERF_FILTER_ERROR: "historicalPerformanceDataList filtering error",
 
-  EXP_AMOUNT_COULDNT_BE_CALC = "expected amount could not be calculated",
-  INTFPMDATAQUALITY_COULDNT_BE_CALC = "interfacePmDataQuality could not be calculated",
+  UPDATE_NMRPET_ERROR: "updateNewMostRecentPeriodEndTime update error",
+  NUMBER_RECV_15PM_COULDNT_UPDATE: "number of received 15min PM records could not be updated",
+
+  // Internal processing
+  RECV_NOT_UPDATED: "received not provided",
+  RECV_INVALID: "received invalid",
+  GRAN_NOT_PROVIDED: "granularityPeriod not provided",
+  GRAN_INVALID: "granularityPeriod invalid",
+  PERIOD_END_TIME_NOT_PROV: "periodEndTime not provided",
+  PERIOD_END_TIME_INVALID: "periodEndTime invalid",
+
+  NMRPET_NOT_PROVIDED: "newMostRecentPeriodEndTime not provided",
+  NMRPET_INVALID: "newMostRecentPeriodEndTime invalid",
+  NMRPET24_NOT_PROVIDED: "newMostRecentPeriodEndTime24 not provided",
+  NMRPET24_INVALID: "newMostRecentPeriodEndTime24 invalid",
+  NMRPET_COULDNT_PROVIDE: "newMostRecentPeriodEndTime could not be provided",
 
   GENERAL_ERROR: "General processing error"
 }
 
 module.exports = ERRORS;
-
