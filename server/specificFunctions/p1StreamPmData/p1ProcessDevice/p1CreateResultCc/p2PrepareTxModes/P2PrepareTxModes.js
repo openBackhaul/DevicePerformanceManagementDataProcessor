d69216c9e3b6a3a3d44537a88030be1143ecefd8
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-06 11:55:05 +0200

    Adding scafolding for p2PrepareTxModes

---
+++ server/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p2PrepareTxModes/P2PrepareTxModes.js
@@ -0,0 +1,14 @@
+const ERRORS = require("./ErrorsEnum");
+
+function p2PrepareTxModes(input) {
+  try {
+
+  } catch (error) {
+    return ERRORS.GENERAL_ERROR;
+  }
+}
+
+
+
+
+module.exports = p2PrepareTxModes;
