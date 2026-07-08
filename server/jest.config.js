23649d1e56c974e309bd1f0610dcffce70c9a613
KarthikeyanV <karthikeyan.vadivelu@techmahindra.com>
2026-07-08 14:22:21 +0200

    Added unit testcases

--- server/jest.config.js
+++ server/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     '<rootDir>/genericFunctions/p1CalculateEthernetKpis',
     '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputApt',
     '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1FormattingOutputOnf',
+    '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc',
     '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1PrepareTxModes',
     '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1IterateAiPmSlices',
     '<rootDir>/specificFunctions/p1StreamPmData/p1ProcessDevice/p1CreateResultCc/p1IterateEcPmSlices',
