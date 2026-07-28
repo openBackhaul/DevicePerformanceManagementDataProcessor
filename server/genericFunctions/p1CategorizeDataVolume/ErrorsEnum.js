3cae0010b3c3e68b3af7f2bb025e6de465ee8327
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-28 09:19:31 +0200

    Adding function: p1CategorizeDataVolume To Be implement function p1CategorizeDataVolume Fixes #242

---
+++ server/genericFunctions/p1CategorizeDataVolume/ErrorsEnum.js
@@ -0,0 +1,14 @@
+const ERRORS = {
+  HISTORICAL_PERF_NOT_PROVIDED: "historicalPerformanceDataList not provided",
+  HISTORICAL_PERF_INVALID: "historicalPerformanceDataList invalid",
+
+  INTERFACE_STATUS_NOT_PROVVIDED: "interfaceStatus not provided",
+  INTERFACE_STATUS_INVALID: "interfaceStatus invalid",
+
+  PM_DATA_WRONG_GRAN_PROV: "PM data of wrong granularity provided",
+  PM_DATA_CATEG_FAILED: "PM data categorization failed",
+
+  GENERAL_ERROR: "General processing error"
+}
+
+module.exports = ERRORS;
