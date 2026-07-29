986fbef7abc3a7ac365b9a016f27484e0b07713b
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-07-29 13:18:15 +0200

    Add package.json, business logic and errors enumeration

---
+++ server/genericFunctions/p1DocumentFunction/ErrorsEnum.js
@@ -0,0 +1,10 @@
+const ERRORS = {
+  PARAMETERS_NOT_PROVIDED: "parameters not provided",
+  PARAMETERS_INVALID: "parameters invalid",
+
+  DOC_COULDNT_CREATED: "documentation could not be created",
+
+  GENERAL_ERROR: "General processing error"
+}
+
+module.exports = ERRORS;
